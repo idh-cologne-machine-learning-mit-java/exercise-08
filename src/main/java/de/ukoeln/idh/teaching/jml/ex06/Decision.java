c2460471471a0d558a354135847822bd1a4e6ada
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2020-12-07 15:54:54 +0100

    added empty functions

---
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex06/Decision.java
@@ -0,0 +1,13 @@
+package de.ukoeln.idh.teaching.jml.ex06;
+
+import weka.core.Instance;
+
+public class Decision {
+	int attributeIndex = -1;
+	double targetClass = -1;
+
+	public int decide(Instance instance) {
+		return -1;
+	}
+
+}
---
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex06/Trainer.java
@@ -0,0 +1,9 @@
+package de.ukoeln.idh.teaching.jml.ex06;
+
+import weka.core.Instances;
+
+public class Trainer {
+	public DecisionTree train(Instances instances) {
+		return null;
+	}
+}
