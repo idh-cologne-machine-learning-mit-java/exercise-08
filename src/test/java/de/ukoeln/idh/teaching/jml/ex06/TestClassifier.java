0f066eb22e3e42c2a6857c4be6037062668abf64
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2020-12-08 13:40:41 +0100

    foundation for unit tests

---
+++ src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestClassifier.java
@@ -0,0 +1,24 @@
+package de.ukoeln.idh.teaching.jml.ex06;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestClassifier {
+
+	@BeforeEach
+	public void setup() {
+
+	}
+
+	@Test
+	public void testEntropy() {
+		assertTrue(true); // <-- this needs to be changed
+	}
+
+	@Test
+	public void testInformationGain() {
+		assertTrue(true); // <-- this needs to be changed
+	}
+}
