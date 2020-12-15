39fe76919eb33a61d59891037bf877873ac9c4ab
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2020-12-15 03:18:31 +0100

    reference solution

--- src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestTree.java
+++ src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestTree.java
@@ -1,11 +1,14 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestTree {
+	Tree tree = new Tree();
+
 	@BeforeEach
 	public void setup() {
 
@@ -13,7 +16,14 @@ public class TestTree {
 
 	@Test
 	public void testIsLeaf() {
-		assertTrue(true); // <-- this needs to be changed
+		assertTrue(tree.isLeaf());
+
+		tree.children = new Tree[] {};
+		assertTrue(tree.isLeaf());
+
+		tree.children = new Tree[] { new Tree(), new Tree() };
+		assertFalse(tree.isLeaf());
+
 	}
 
 }
