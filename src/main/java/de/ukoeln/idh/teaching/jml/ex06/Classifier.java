5b37045e5631b9c9962ef08e2d9a73bb9cc2ae92
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2020-12-21 12:18:38 +0100

    Calculate entropy from an int[]

--- src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
@@ -35,4 +35,40 @@ public class Classifier {
 	protected double log(double a, double b) {
 		return Math.log10(a) / Math.log10(b);
 	}
+
+	/**
+	 * This is the core method to calculate entropy. Since it's only based on
+	 * relative frequencies, we can calculate it without knowing the classes
+	 * themselves, just using the numbers per class.
+	 * 
+	 * @param numberOfInstances An int array that contains one entry per class. The
+	 *                          entry represents the number of instances for this
+	 *                          class.
+	 * @return The entropy of the given data set.
+	 */
+	protected double entropy(int[] numberOfInstances) {
+		// to calculate relative frequency, we need the total number of instances
+		int total = 0;
+		// do a loop and add up the total number
+		for (int i = 0; i < numberOfInstances.length; i++) {
+			total += numberOfInstances[i];
+		}
+
+		// placeholder for the return value
+		double ret = 0.0;
+
+		// iterate over each class
+		for (int i = 0; i < numberOfInstances.length; i++) {
+			// to avoid taking the log of 0, we skip non-used classes
+			// (in my implementation of information gain, they might come up)
+			if (numberOfInstances[i] == 0)
+				continue;
+
+			// calculate relative frequency of this class
+			double rfreq = numberOfInstances[i] / (double) total;
+			ret += rfreq * log(rfreq, Math.E);
+		}
+		// return the return value
+		return -1 * ret;
+	}
 }
