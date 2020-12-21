243f0ead27c367db49063134f410acfefb20ac06
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2020-12-21 12:18:02 +0100

    Added helper function to calculate log with arbitrary bases

--- src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
@@ -21,4 +21,18 @@ public class Classifier {
 		// TODO: implement
 		return 0.0;
 	}
+
+	/**
+	 * This method uses the math rules that allows to change the base via
+	 * mathematical operations: log(x, base) = log(x, 10) / log(base, 10). Since
+	 * we're using this a few times, wrapping it in a simple function. Source:
+	 * https://en.wikipedia.org/wiki/List_of_logarithmic_identities#Changing_the_base
+	 * 
+	 * @param a The value for which to calculate the logarithm
+	 * @param b The base of the logarithm
+	 * @return The logarithm of a to base b
+	 */
+	protected double log(double a, double b) {
+		return Math.log10(a) / Math.log10(b);
+	}
 }
