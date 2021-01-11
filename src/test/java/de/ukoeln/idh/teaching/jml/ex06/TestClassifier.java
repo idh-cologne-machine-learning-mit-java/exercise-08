3e62319aaeae4d2f9721bc7281c7bc0a1570db97
Cosima Granderath-Miegel <cgrander@smail.uni-koeln.de>
2021-01-11 15:02:15 +0100

    test

--- src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestClassifier.java
+++ src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestClassifier.java
@@ -1,111 +1,80 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import weka.core.Attribute;
-import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 
 public class TestClassifier {
 
-	Random random = new Random();
 	Classifier classifier = new Classifier();
+	Instances data;
+	Tree results;
+
 
 	@BeforeEach
-	public void setup() throws FileNotFoundException, IOException {
-	}
-
-	/**
-	 * This method is used to generate a data set with a specific distribution. For
-	 * calculating entropy, the number of classes and their distribution is the
-	 * deciding factor. The returned data set contains 100 instances.
-	 * 
-	 * @param numberOfClasses The number of classes
-	 * @param portions        An array giving the absolute number of each of the
-	 *                        different classes. The values in the array should add
-	 *                        up to 100.
-	 * @return A data set containing 100 instances.
-	 * @throws IOException If the base arff file can't be loaded
-	 */
-	public Instances generateDataset(int numberOfClasses, int[] portions) throws IOException {
-
-		// We first generate possible class values
-		List<String> attributeValues = new ArrayList<String>();
-		for (int i = 0; i < numberOfClasses; i++) {
-			attributeValues.add("Class " + i);
+	public void setup() throws Exception{
+		DataSource source = new DataSource("src/resources/germancredit/train.arff");
+		data = source.getDataSet();
+		if(data.classIndex() == -1){
+			data.setClassIndex(data.numAttributes() - 1);
 		}
-
-		// and assign the created class values to a new attribute called class
-		Attribute classAttribute = new Attribute("class", attributeValues);
-
-		// Load the base data set, and add the new attribute, set it as class attribute
-		// Adding a new attribute sets all values to "unknown", we have to fix that
-		// later
-		Instances instances = new Instances(new FileReader("src/test/resources/base.arff"));
-		instances.insertAttributeAt(classAttribute, instances.numAttributes());
-		instances.setClassIndex(instances.numAttributes() - 1);
-		// Set the actual class values by "consuming" the given numbers in the portions
-		// array.
-		int currentClass = 0;
-		// iterate over all instances
-		for (Instance instance : instances) {
-			// if there are no instances of the current class to produce anymore, we move on
-			// to the next
-			if (portions[currentClass] == 0) {
-				currentClass++;
-			}
-
-			// set instance value
-			instance.setValue(instances.numAttributes() - 1, currentClass);
-
-			// consume the instance
-			portions[currentClass]--;
-		}
-
-		// shuffle before returning
-		instances.randomize(random);
-
-		return instances;
+		System.out.println(data.numInstances());
 	}
 
 	@Test
-	public void testEntropy() throws IOException {
-		// values taken from slides
-		assertEquals(0.0, classifier.entropy(generateDataset(1, new int[] { 100 })), 1e-03);
-		assertEquals(0.693, classifier.entropy(generateDataset(2, new int[] { 50, 50 })), 1e-03);
-		assertEquals(0.562, classifier.entropy(generateDataset(2, new int[] { 75, 25 })), 1e-03);
-		assertEquals(1.386294, classifier.entropy(generateDataset(4, new int[] { 25, 25, 25, 25 })), 1e-03);
-
-		// values calculated with R:
-		// library(entropy)
-		// entropy(c(75,25))
-		// entropy(c(5, 25, 10, 5, 5, 10, 10, 10, 10, 10))
-		assertEquals(0.5623351, classifier.entropy(generateDataset(2, new int[] { 75, 25 })), 1e-03);
-		assertEquals(2.177484,
-				classifier.entropy(generateDataset(10, new int[] { 5, 25, 10, 5, 5, 10, 10, 10, 10, 10 })), 1e-03);
-
-		Instances instances = new Instances(new FileReader("src/test/resources/ig.arff"));
-		instances.setClassIndex(instances.numAttributes() - 1);
-		assertEquals(0.693, classifier.entropy(instances), 1e-03);
+	public void testTrain(){
+		results = classifier.train(data);
+		assertThrows(IllegalArgumentException.class,()->{classifier.train(null);});
+		
 	}
 
 	@Test
-	public void testInformationGain() throws IOException {
-		Instances instances = new Instances(new FileReader("src/test/resources/ig.arff"));
-		instances.setClassIndex(instances.numAttributes() - 1);
+	public void testEntropy() {
+		//illegal input
+		assertThrows(IllegalArgumentException.class, ()->{classifier.entropy(null);});
+		
+		double H = classifier.entropy(data);
+		
+		assertTrue(0.0 <= H && H <= 2.0);
 
-		assertEquals(0.693, classifier.informationGain(instances, 0), 1e-03);
-		assertEquals(0.693 - 2 * 0.5 * 0.693, classifier.informationGain(instances, 1), 1e-03);
-		assertEquals(0.693 - (0.8 * 0.6615632 + 0.2 * 0), classifier.informationGain(instances, 2), 1e-03);
+	}
+
+	@Test
+	public void testInformationGain() {
+		assertThrows(IllegalArgumentException.class, () -> {classifier.informationGain(null, 0);});
+		assertThrows(IllegalArgumentException.class, () -> {classifier.informationGain(data, -1);});
+		assertThrows(IllegalArgumentException.class, ()-> {classifier.informationGain(data, data.numAttributes()+1);});
+
+		double ig = classifier.informationGain(data, 0);
+		assertTrue(0.0 <= ig && ig <= 1.0);
+	}
+
+	@Test
+	public void testSubsets(){
+		assertThrows(NullPointerException.class, () ->{Classifier.subsets(null, 0);});
+		assertThrows(IndexOutOfBoundsException.class, () -> {Classifier.subsets(data, -1);});
+		assertThrows(IndexOutOfBoundsException.class, () -> {Classifier.subsets(data, data.numAttributes()+1);});
+
+	}
+
+	@Test
+	public void testCountClasses(){
+		assertThrows(NullPointerException.class, () -> {Classifier.countClasses(null);});
+		
+
+		assertEquals(data.numClasses(), Classifier.countClasses(data).length);
+	}
+
+	@Test
+	public void testGetMajority(){
+		assertEquals(0, Classifier.getMajority(new int[] {2,1}));
+		assertEquals(0, Classifier.getMajority(new int[] {2,2}));
+		assertEquals(1, Classifier.getMajority(new int[] {1,2}));
 	}
 }
